71dbf2f682569f55eb2c61f23e4060321039f0c6
zennn312 <zainaulia312@gmail.com>
2026-02-17 20:34:11 +0700

    update regis
    Signed-off-by: zennn312 <zainaulia312@gmail.com>

--- regis.php
+++ regis.php
@@ -1,55 +1,80 @@
 <?php
-session_start();
 require "koneksi.php";
 
-function registrasi($data)
-{
-    global $conn;
-
-    $username = strtolower(stripslashes($data["username"]));
-    $email = strtolower(stripslashes($data["email"]));
-    $password = mysqli_real_escape_string($conn, $data["password"]);
-    $konfirmasi_password = mysqli_real_escape_string($conn, $data["konfirmasi_password"]);
-
-    // cek username sudah ada atau belum
-    $result = mysqli_query($conn, "SELECT username FROM user WHERE username = '$username'");
-
-    if (mysqli_fetch_assoc($result)) {
-        echo "<script>
-                alert('Username sudah terdaftar!');
-              </script>";
-        return false;
-    }
-
-    // cek konfirmasi_password
-    if ($password !== $konfirmasi_password) {
-        echo "<script>
-                alert('Konfirmasi password tidak sesuai!');
-              </script>";
-        return false;
-    }
-
-    // enkripsi password
-    $password = password_hash($password, PASSWORD_DEFAULT);
-
-    // tambahkan user baru ke database
-    mysqli_query($conn, "INSERT INTO user VALUES(NULL, '$username', '$email', '$password')");
-
-    return mysqli_affected_rows($conn);
-}
-
 if (isset($_POST['register'])) {
-    if (registrasi($_POST) > 0) {
-        header("Location: login.php");
-        echo "<script>
-                alert('Registrasi Berhasil!');
-              </script>";
-    } else {
-        echo mysqli_error($conn);
-    }
-}
 
+    // Ambil & bersihkan input
+    $username = trim($_POST["username"]);
+    $email = trim($_POST["email"]);
+    $password = $_POST["password"];
+    $konfirmasi_password = $_POST["konfirmasi_password"];
+
+    // 1. Cek field kosong
+    if (empty($username) || empty($email) || empty($password) || empty($konfirmasi_password)) {
+        echo "<script>alert('Semua field harus diisi!');</script>";
+        exit;
+    }
+
+    // 2. Validasi email
+    if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+        echo "<script>alert('Format email tidak valid!');</script>";
+        exit;
+    }
+
+    // 3. Validasi panjang password
+    if (strlen($password) < 8) {
+        echo "<script>alert('Password minimal 8 karakter!');</script>";
+        exit;
+    }
+
+    // 4. Validasi kompleksitas password
+    if (
+        !preg_match("/[a-z]/", $password) ||
+        !preg_match("/[A-Z]/", $password) ||
+        !preg_match("/[0-9]/", $password) ||
+        !preg_match("/[!@#$%^&*()_+]/", $password)
+    ) {
+        echo "<script>alert('Password harus mengandung huruf besar, kecil, angka, dan simbol!');</script>";
+        exit;
+    }
+
+    // 5. Cek konfirmasi password
+    if ($password !== $konfirmasi_password) {
+        echo "<script>alert('Konfirmasi password tidak sesuai!');</script>";
+        exit;
+    }
+
+    // 6. Cek username sudah ada atau belum (Prepared Statement)
+    $stmt = mysqli_prepare($conn, "SELECT id_user FROM user WHERE username = ?");
+    mysqli_stmt_bind_param($stmt, "s", $username);
+    mysqli_stmt_execute($stmt);
+    mysqli_stmt_store_result($stmt);
+
+    if (mysqli_stmt_num_rows($stmt) > 0) {
+        echo "<script>alert('Username sudah terdaftar!');</script>";
+        exit;
+    }
+
+    mysqli_stmt_close($stmt);
+
+    // 7. Hash password
+    $hashed_password = password_hash($password, PASSWORD_DEFAULT);
+
+    // 8. Insert user baru
+    $stmt = mysqli_prepare($conn, "INSERT INTO user (username, email, password) VALUES (?, ?, ?)");
+    mysqli_stmt_bind_param($stmt, "sss", $username, $email, $hashed_password);
+
+    if (mysqli_stmt_execute($stmt)) {
+        echo "<script>alert('Registrasi berhasil!');</script>";
+    } else {
+        echo "<script>alert('Terjadi kesalahan saat registrasi!');</script>";
+    }
+
+    mysqli_stmt_close($stmt);
+}
 ?>
+
+
 
 
 <!DOCTYPE html>
@@ -66,28 +91,28 @@ if (isset($_POST['register'])) {
 
     <form action="" method="post">
 
-    <ul>
-        <li>
-            <label for="username">Username : </label>
-            <input type="text" name="username" id="username">
-        </li>
-        <li>
-            <label for="email">E-mail : </label>
-            <input type="text" name="email" id="email">
-        </li>
-        <li>
-            <label for="password">Password : </label>
-            <input type="password" name="password" id="password">
-        </li>
-        <li>
-            <label for="konfirmasi_password">Konfirmasi Password : </label>
-            <input type="password" name="konfirmasi_password" id="konfirmasi_password">
-        </li>
-        <li>
-            <button type="submit" name="register">Register</button>
-            <a href="login.php">Login</a>
-        </li>
-    </ul>
+        <ul>
+            <li>
+                <label for="username">Username : </label>
+                <input type="text" name="username" id="username">
+            </li>
+            <li>
+                <label for="email">E-mail : </label>
+                <input type="text" name="email" id="email">
+            </li>
+            <li>
+                <label for="password">Password : </label>
+                <input type="password" name="password" id="password">
+            </li>
+            <li>
+                <label for="konfirmasi_password">Konfirmasi Password : </label>
+                <input type="password" name="konfirmasi_password" id="konfirmasi_password">
+            </li>
+            <li>
+                <button type="submit" name="register">Register</button>
+                <a href="login.php">Login</a>
+            </li>
+        </ul>
     </form>
 </body>
 
