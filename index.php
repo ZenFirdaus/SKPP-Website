e7a3aa0aec6e4e618d93e060705439bd503ecf73
zennn312 <zainaulia312@gmail.com>
2026-02-19 10:37:44 +0700

    update index.php
    Signed-off-by: zennn312 <zainaulia312@gmail.com>

--- index.php
+++ index.php
@@ -1,63 +1,92 @@
 <?php
 session_start();
-include "koneksi.php";
-if (!isset($_SESSION['username'])) {
+require "koneksi.php";
+
+if (!isset($_SESSION['login'])) {
     header("Location: login.php");
     exit;
 }
+
 ?>
 
 <!DOCTYPE html>
-<html lang="en">
+<html lang="id">
 
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Halaman Dasboard</title>
+    <title>SIMUDAH Dashboard</title>
+
+    <!-- Google Font -->
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700&display=swap" rel="stylesheet">
+
+    <link rel="stylesheet" href="index.css">
+    
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
 </head>
 
 <body>
-    <nav class="navbar bg-body-tertiary fixed-top">
-        <div class="container-fluid">
-            <a class="navbar-brand" href="#">Offcanvas navbar</a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="offcanvas offcanvas-end" tabindex="-1" id="offcanvasNavbar" aria-labelledby="offcanvasNavbarLabel">
-                <div class="offcanvas-header">
-                    <h5 class="offcanvas-title" id="offcanvasNavbarLabel">Offcanvas</h5>
-                    <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+
+    <div class="mobile-container">
+
+        <div class="header">
+            <div class="header-top">
+                <div class="profile">
+                    <div class="profile-icon"></div>
+                    <div class="username">Halo, Username</div>
                 </div>
-                <div class="offcanvas-body">
-                    <ul class="navbar-nav justify-content-end flex-grow-1 pe-3">
-                        <li class="nav-item">
-                            <a class="nav-link active" aria-current="page" href="#">Home</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="#">Link</a>
-                        </li>
-                        <li class="nav-item dropdown">
-                            <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                                Dropdown
-                            </a>
-                            <ul class="dropdown-menu">
-                                <li><a class="dropdown-item" href="#">Action</a></li>
-                                <li><a class="dropdown-item" href="#">Another action</a></li>
-                                <li>
-                                    <hr class="dropdown-divider">
-                                </li>
-                                <li><a class="dropdown-item" href="#">Something else here</a></li>
-                            </ul>
-                        </li>
-                    </ul>
-                    <form class="d-flex mt-3" role="search">
-                        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
-                        <button class="btn btn-outline-success" type="submit">Search</button>
-                    </form>
-                </div>
+                <div>⚙️ ⋮</div>
             </div>
         </div>
-    </nav>
+
+        <div class="welcome-card">
+            <h1>Selamat Datang di SIMUDAH</h1>
+            <p>Pengajuan, pengecekan, hingga arsip dalam satu aplikasi</p>
+        </div>
+
+        <div class="menu-grid">
+            <div class="menu-item pengajuan">
+                <div class="icon-placeholder"></div>
+                Pengajuan
+            </div>
+
+            <div class="menu-item catat">
+                <div class="icon-placeholder"></div>
+                Catat Data
+            </div>
+
+            <div class="menu-item cek">
+                <div class="icon-placeholder"></div>
+                Pengecekan
+            </div>
+
+            <div class="menu-item draft">
+                <div class="icon-placeholder"></div>
+                Draft SKPP
+            </div>
+
+            <div class="menu-item arsip">
+                <div class="icon-placeholder"></div>
+                Arsip
+            </div>
+
+            <div class="menu-item unduh">
+                <div class="icon-placeholder"></div>
+                Unduh SKPP
+            </div>
+
+        </div>
+
+    </div>
+
+    <div class="bottom-nav">
+        <div class="nav-icon active">
+            <i class="bi bi-house-door-fill"></i>
+        </div>
+        <div class="nav-icon">➕</div>
+        <div class="nav-icon">👤</div>
+    </div>
+
 </body>
 
 </html>
